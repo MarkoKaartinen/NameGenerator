85e234cef0ff29e7303bfc661adae40c0a8a2e5c
Marko <markokaartinen@gmail.com>
2011-04-01 19:43:20 +0300

    trimmed and stripped some slashes

--- generate.php
+++ generate.php
@@ -3,7 +3,7 @@ $lang = $_GET['lang'];
 $female = $_GET['fe'];
 $male = $_GET['ma'];
 $all = $_GET['all'];
-$syntax = htmlentities(urldecode($_GET['s']));
+$syntax = stripslashes(htmlentities(urldecode($_GET['s'])));
 
 $women = file("names/$lang/female_firstnames.txt");
 $men = file("names/$lang/male_firstnames.txt");
@@ -15,9 +15,9 @@ $i = 0;
 while($i < $all){
 	for($j = 0; $j < $female; $j++){
 		shuffle($women);
-		$first = $women[0];
+		$first = trim($women[0]);
 		shuffle($last);
-		$sur = $last[0];
+		$sur = trim($last[0]);
 		$str = "";
 		$str = str_replace("[FIRST]", $first, $syntax);
 		$str = str_replace("[LAST]", $sur, $str);
@@ -26,9 +26,9 @@ while($i < $all){
 	}
 	for($j = 0; $j < $male; $j++){
 		shuffle($men);
-		$first = $men[0];
+		$first = trim($men[0]);
 		shuffle($last);
-		$sur = $last[0];
+		$sur = trim($last[0]);
 		$str = "";
 		$str = str_replace("[FIRST]", $first, $syntax);
 		$str = str_replace("[LAST]", $sur, $str);
@@ -39,13 +39,13 @@ while($i < $all){
 		$rnd = rand(0, 1);
 		if($rnd == 0){
 			shuffle($men);
-			$first = $men[0];
+			$first = trim($men[0]);
 		}else{
 			shuffle($women);
-			$first = $women[0];
+			$first = trim($women[0]);
 		}
 		shuffle($last);
-		$sur = $last[0];
+		$sur = trim($last[0]);
 		$str = "";
 		$str = str_replace("[FIRST]", $first, $syntax);
 		$str = str_replace("[LAST]", $sur, $str);
